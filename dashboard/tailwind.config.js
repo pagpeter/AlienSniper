d27400e0578d62d6a2b88ef8e2ee2ff749c97710
peet <peet@you.gay>
2022-01-19 20:35:23 +0100

    New Vue project

---
+++ dashboard/tailwind.config.js
@@ -0,0 +1,9 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  presets: [],
+  darkMode: false, // or 'media' or 'class'
+  mode: 'jit',
+  plugins: [],
+}
