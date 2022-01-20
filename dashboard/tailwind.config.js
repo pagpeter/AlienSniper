a047dec78c9d9c36d5340baab9e51f5097940770
peet <peet@you.gay>
2022-01-20 18:16:18 +0100

    Added basic dashboard, no functionality

--- dashboard/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-const colors = require('tailwindcss/colors')
-
-module.exports = {
-  purge: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  presets: [],
-  darkMode: false, // or 'media' or 'class'
-  mode: 'jit',
-  plugins: [],
-}
